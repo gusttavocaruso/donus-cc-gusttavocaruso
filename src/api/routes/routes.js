af5a8add39e57d4ce2fdd2f20204d19c324ee1c4
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-01-25 20:22:37 -0300

    trying

--- src/api/routes/routes.js
+++ src/api/routes/routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const res = require('express/lib/response');
 const { registerAccount, newDeposit, getAccounts,
   newTransfer } = require('../controllers/account.controllers');
 
 const router = Router();
 
+router.get('/', () => res.status(200).json({ message: 'hlwrd' }));
 router.post('/register', registerAccount);
 router.get('/accounts', getAccounts);
 router.put('/deposit', newDeposit);
